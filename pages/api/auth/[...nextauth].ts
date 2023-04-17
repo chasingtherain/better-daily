7647155ce2c054a2a6c5beb24e054aea8605751c
ChasingRain <junpeng94@gmail.com>
2023-04-17 15:53:03 +0800

    added facebook auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,6 +3,7 @@
 import { NextApiHandler } from 'next';
 import NextAuth from 'next-auth';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import FacebookProvider from "next-auth/providers/facebook";
 import GitHubProvider from 'next-auth/providers/github';
 import GoogleProvider from "next-auth/providers/google";
 import prisma from '../../../lib/prisma';
@@ -12,6 +13,10 @@ export default authHandler;
 
 const options = {
   providers: [
+    FacebookProvider({
+        clientId: process.env.FACEBOOK_ID,
+        clientSecret: process.env.FACEBOOK_SECRET
+    }),
     GitHubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
