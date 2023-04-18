c24806ed11af82dbcfe94d4aef79e6d5b6b8bbee
ChasingRain <junpeng94@gmail.com>
2023-04-18 11:13:39 +0800

    updated profile page css

--- pages/profile.js
+++ pages/profile.js
@@ -7,17 +7,19 @@ export default function Profile() {
 
   if (session) {
     return (
-        <div className="flex flex-wrap items-center gap-[15px] px-5">
-            <Label.Root className="text-[15px] font-medium leading-[35px]" htmlFor="firstName">
-            Email
-            </Label.Root>
-            <input
-            className="bg-blackA5 shadow-blackA9 inline-flex h-[35px] w-[200px] appearance-none items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none shadow-[0_0_0_1px] outline-none focus:shadow-[0_0_0_2px_black] selection:color-white selection:bg-blackA9"
-            type="text"
-            id="firstName"
-            disabled
-            defaultValue={session.user.email}
-            />
+        <div className="flex flex-col items-center gap-5 mt-10 md:mt-20">
+            <div className='flex items-center gap-[15px] px-5'>
+                <Label.Root className="text-[15px] font-medium leading-[35px]" htmlFor="email">
+                Email
+                </Label.Root>
+                <input
+                className="bg-blackA5 shadow-blackA9 inline-flex h-[35px] w-[200px] appearance-none items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none shadow-[0_0_0_1px] outline-none focus:shadow-[0_0_0_2px_black] selection:color-white selection:bg-blackA9"
+                type="text"
+                id="email"
+                disabled
+                defaultValue={session.user.email}
+                />
+            </div>
             <ActionButton name="Log Out" action={() => signOut({ callbackUrl: 'http://localhost:3000/' })}/>
       </div>
     )
