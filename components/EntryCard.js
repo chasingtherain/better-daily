50fbb415c2ce34477a5963fdbe0a00d0eefa7696
ChasingRain <junpeng94@gmail.com>
2023-04-19 12:45:42 +0800

    successfully fetched data from db

--- components/EntryCard.js
+++ components/EntryCard.js
@@ -2,111 +2,66 @@ import { Button } from "@/components/ui/button"
 import {
   Card,
   CardContent,
-  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Separator } from "@/components/ui/separator"
-import { useSession } from "next-auth/react"
-import React, { useEffect, useState } from "react"
+import React from "react"
 
-export function EntryCard() {
-    const { data: session, status } = useSession()
-    const [entries, setEntries] = useState([])
-    const [isLoading, setIsLoading] = useState(true)
-
-    useEffect(() => {
-    // Fetch data from the database here
-    const fetchData = async () => {
-        try {
-            const response = await fetch(`/api/entry/get/all?params=${session.user.email}`);
-            const jsonData = await response.json();
-
-            setEntries(jsonData.entries);
-            setIsLoading(false)
-        } 
-        catch (error) {
-            console.error('Error fetching data:', error);
-        }
-    };
-
-    fetchData();
-    }, []); // Empty dependency array to run effect only on component mount
-
+export function EntryCard({entry}) {
     return (
-    <Card className="md:w-1/2 border-2">
+    <Card className="w-4/5 md:w-[400px] h-[400px] border-2 my-4">
         <CardHeader>
-        <CardTitle>Date</CardTitle>
+        <CardTitle>{entry.todayDate}</CardTitle>
         </CardHeader>
         <CardContent>
             <div className="grid md:w-full items-center gap-3">
-                {!isLoading && entries?.map((entry) => {
                     <React.Fragment>
                         <div className="flex flex-col space-y-1.5">
                             <Label htmlFor="name">I was thankful for</Label>
-                            <React.Fragment>
-                            <div className="text-sm">
-                                {entry.gratefulContent[0]}
-                            </div>
                             <Separator className="my-2 bg-slate-200" />
-                            <div className="text-sm">
-                                {entry.gratefulContent[1]}
-                            </div>
-                            <Separator className="my-2 bg-slate-200" />
-                            <div className="text-sm">
-                                {entry.gratefulContent[2]}
-                            </div>
-                            <Separator className="my-2 bg-slate-200" />
-                            </React.Fragment>
+                            {entry.gratefulContent.map((content) => 
+                                <React.Fragment key={content}>
+                                    <div className="text-sm">
+                                        {content}
+                                    </div>
+                                </React.Fragment>
+                            )}
                         </div>
                         <div className="flex flex-col space-y-1.5">
                             <Label htmlFor="name">These went well</Label>
-                        <React.Fragment>
-                            <div className="text-sm">
-                                {entry.wentWellContent[0]}
-                            </div>
                             <Separator className="my-2 bg-slate-200" />
-                            <div className="text-sm">
-                                {entry.wentWellContent[1]}
-                            </div>
-                            <Separator className="my-2 bg-slate-200" />
-                            <div className="text-sm">
-                                {entry.wentWellContent[2]}
-                            </div>
-                            <Separator className="my-2 bg-slate-200" />
-                            </React.Fragment>
+                            {entry.wentWellContent.map(content => (
+                                <React.Fragment key={content}>
+                                    <div className="text-sm" >
+                                        {content}
+                                    </div>
+                                </React.Fragment>
+                            ))}
                         </div>
                         <div className="flex flex-col space-y-1.5">
                             <Label htmlFor="name">These didn't go as expected</Label>
-                            <React.Fragment>
-                            <div className="text-sm">
-                                {entry.notSoWellContent[0]}
-                            </div>
                             <Separator className="my-2 bg-slate-200" />
-                            <div className="text-sm">
-                                {entry.notSoWellContent[1]}
-                            </div>
-                            <Separator className="my-2 bg-slate-200" />
-                            <div className="text-sm">
-                                {entry.notSoWellContent[2]}
-                            </div>
-                            <Separator className="my-2 bg-slate-200" />
-                            </React.Fragment>
+                            {entry.notSoWellContent.map((content) => {
+                                <React.Fragment key={content}>
+                                    <div className="text-sm">
+                                        {content}
+                                    </div>
+                                </React.Fragment>
+                            })}
                         </div>
                         <div className="flex flex-col space-y-1.5">
                             <Label htmlFor="name">I could improve on</Label>
+                            <Separator className="my-2 bg-slate-200" />
                             <React.Fragment>
                             <div className="text-sm">
                                 {entry.improvementContent[0]}
                             </div>
-                            <Separator className="my-2 bg-slate-200" />
                             </React.Fragment>
                         </div>
                     </React.Fragment>
-                })}
             </div>
         </CardContent>
         <CardFooter className="flex justify-between">
--- pages/entry/all.js
+++ pages/entry/all.js
@@ -1,18 +1,41 @@
 import { getSession, useSession } from "next-auth/react"
+import { useEffect, useState } from "react"
 import { EntryCard } from "../../components/EntryCard"
 
 export default function Home() {
   const { data: session, status } = useSession()
   const loading = status === "loading"
+  const [entries, setEntries] = useState([])
+  console.log(session)
+    useEffect(() => {
+    // Fetch data from the database here
+    const fetchData = async () => {
+        try {
+          if (session && session.user && session.user.email){
+            const response = await fetch(`/api/entry/get/all?params=${session.user.email}`);
+            console.log("response: ", response)
+            const jsonData = await response.json();
+
+            setEntries(jsonData.entries);
+          }
+        } 
+        catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+    fetchData();
+    }, [session]); // Empty dependency array to run effect only on component mount
+
 
   if (typeof window !== "undefined" && loading) return null
   // console.log(session,status)
+
   if (session && status=="authenticated") {
     return (
-      <>
-        <h1>All Entries</h1>
-        <EntryCard/>
-      </>
+      <div className="flex flex-wrap justify-center items-center gap-3">
+        {entries.map(entry => <EntryCard key={entry.id} entry={entry} />)}
+      </div>
     )
   }
     // return <p>Access Denied</p>
