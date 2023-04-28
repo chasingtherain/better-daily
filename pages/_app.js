b507a02384cd9dea8737596766feaac8636e2ccd
ChasingRain <junpeng94@gmail.com>
2023-04-28 16:39:23 +0800

    added mobile landing page

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,6 @@ import Navbar from '../components/Navbar'
 import { ThemeProvider } from 'next-themes'
 
 export default function App({ Component, pageProps }) {
-  const [theme, setTheme] = useState('dark')
 
   return <Fragment>
     <Head>
--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,12 @@ import ActionButton from "../components/ui/ActionButton"
 import { authOptions } from 'pages/api/auth/[...nextauth]'
 import { getServerSession } from "next-auth/next"
 import { quotes } from "../data/quotes"
-import { Icons } from "../components/icons"
+import {BrowserView, MobileView, isBrowser, isMobile} from 'react-device-detect';
+import WebLanding from "../components/landing/WebLanding"
 
 export default function Home(props) {
   const { data: session, status } = useSession()
-  console.log(props)
+  
   if (session) {
     return (
       <div className="h-screen border-2 border-transparent">
@@ -23,80 +24,76 @@ export default function Home(props) {
       </div>
     )
   }
+
   if(!session){
-    return (
-      <>
-        <div className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-  
-          <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex">
-            <div
-              className="absolute inset-0 bg-cover"
-              style={{
-                backgroundImage:
-                  "url(images/landing/night-man-alone-starry-sky-night-sky-comet-silhouette-3840x2160-8325.jpg)",
-                  backgroundPositionX: "-200px", // Specify pixel x position here
-                  backgroundPositionY: "-75px", // Specify pixel x position here
-                  height: "100vh"
-                }}
-            />
-            <div className="relative z-20 flex items-center text-lg font-medium">
-              <Command className="mr-2 h-6 w-6" /> Get Better Daily
-            </div>
-            <div className="relative z-20 mt-[100%]">
-              <blockquote className="space-y-2">
-                <p className="text-lg italic">
-                  The unexamined life is not worth living
-                </p>
-                <footer className="text-sm">Socrates</footer>
-              </blockquote>
-            </div>
+    return <>
+      <div className={`flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0`}>
+        <div className="relative h-full flex-col bg-muted p-10 text-white lg:flex">
+          <div
+            className="absolute inset-0 bg-cover"
+            style={{
+              backgroundImage:
+                "url(images/landing/night-man-alone-starry-sky-night-sky-comet-silhouette-3840x2160-8325.jpg)",
+                backgroundPositionX: "-200px", // Specify pixel x position here
+                backgroundPositionY: "-75px", // Specify pixel x position here
+                height: "100vh"
+              }}
+          />
+          <div className="relative z-20 flex items-center text-lg font-medium">
+            <Command className="mr-2 h-6 w-6" /> Get Better Daily
           </div>
-          <div className="lg:p-8">
-            <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[300px] px-4">
-              <div className="flex flex-col space-y-2 text-center">
-                <h1 className="text-2xl font-semibold tracking-tight my-2">
-                  Start My Journey
-                </h1>
-                <ActionButton name="Continue"/>
-              </div>
-              <p className="px-8 text-center text-sm text-muted-foreground">
-                By proceeding, you agree to our{" "}
-                <Link
-                  href="/terms"
-                  className="underline underline-offset-4 hover:text-primary"
-                >
-                  Terms of Service
-                </Link>{" "}
-                and{" "}
-                <Link
-                  href="/privacy"
-                  className="underline underline-offset-4 hover:text-primary"
-                >
-                  Privacy Policy
-                </Link>
-                .
+          <div className={isBrowser ? "relative z-20 mt-[100%]" : "relative mb-0"}>
+            <blockquote className="space-y-2">
+              <p className="text-lg italic">
+                The unexamined life is not worth living
               </p>
-            </div>
+              <footer className="text-sm">Socrates</footer>
+            </blockquote>
           </div>
         </div>
+        <div className="lg:p-8">
+          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[300px] px-4">
+            <div className="flex flex-col space-y-2 text-center">
+              <h1 className="text-2xl font-semibold tracking-tight my-2">
+                Introspect Today
+              </h1>
+              <ActionButton name="Start Journaling"/>
+            </div>
+            <p className="px-8 text-center text-sm text-muted-foreground">
+              By proceeding, you agree to our{" "}
+              <Link
+                href="/terms"
+                className="underline underline-offset-4 hover:text-primary"
+              >
+                Terms of Service
+              </Link>{" "}
+              and{" "}
+              <Link
+                href="/privacy"
+                className="underline underline-offset-4 hover:text-primary"
+              >
+                Privacy Policy
+              </Link>
+              .
+            </p>
+          </div>
+        </div>
+      </div>
       </>
-    )
   }
-}
+
+  }
 
 export async function getServerSideProps(context){
   const session = await getServerSession(context.req, context.res, authOptions)
-  // console.log("session from gssp: ", session)
-
   const randomNum = Math.floor(Math.random() * quotes.length)
   const chosenQuote = quotes.filter(quote => quote.id === randomNum)[0]
-  console.log(chosenQuote)
-
 
   return {
     props: {
       session,
       quote: chosenQuote,
+      isMobile: isMobile
     },
   }
 }
