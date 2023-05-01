aaa54c190110adc02e48057ce4fdf3a0b00ffea4
ChasingRain <junpeng94@gmail.com>
2023-05-01 16:10:55 +0800

    converted component files to ts or tsx

--- components/EntryCard.tsx
+++ components/EntryCard.tsx
@@ -1,9 +1,7 @@
 import React from "react"
-import { Button } from "@/components/ui/button"
 import {
   Card,
   CardContent,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
