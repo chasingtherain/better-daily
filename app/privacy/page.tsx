efb7d79a35e45899aa24a2dfb6b79c73c000f236
ChasingRain <junpeng94@gmail.com>
2023-05-12 12:59:48 +0800

    added app dir and added privacy and terms page to it

---
+++ app/privacy/page.tsx
@@ -0,0 +1,47 @@
+
+export default function Privacy() {
+    return (
+        <div className="mx-5 mt-5 md:mx-10 md:mt-10 space-y-2">
+            <h1>Privacy Policy of Better Daily</h1>
+
+            <h2>1. Information We Collect</h2>
+
+            <h3>1.1 Personal Information:</h3>
+            <p>We do not collect any personally identifiable information, such as your name, email address, or contact details. "Better Daily" is designed to be an anonymous and private journaling experience.</p>
+
+            <h3>1.2 Journal Entries:</h3>
+            <p>The app allows you to create journal entries where you can reflect on various aspects of your life. These entries are not shared with any third parties.</p>
+
+            <h3>1.3 Usage Data:</h3>
+            <p>We may collect non-personal information about how you use the app, such as the features you access, the frequency and duration of your app usage, and any errors or crashes that occur. This information is collected to improve app performance and enhance user experience, but it does not identify you personally.</p>
+
+            <h2>2. Use of Information</h2>
+
+            <h3>2.1 Personal Information:</h3>
+            <p>As stated earlier, we do not collect any personal information. Therefore, we do not use your personal information for any purpose.</p>
+
+            <h3>2.2 Usage Data:</h3>
+            <p>We may use the non-personal usage data to analyze trends, track app performance, and improve the app's functionality and features. This data is anonymized and aggregated, meaning it does not identify you individually.</p>
+
+            <h2>3. Disclosure of Information</h2>
+
+            <h3>3.1 Personal Information:</h3>
+            <p>We do not disclose it to any third parties.</p>
+
+            <h3>3.2 Usage Data:</h3>
+            <p>We may share non-personal usage data with trusted third-party service providers who assist us in analyzing app performance and improving user experience. However, this data does not identify you personally.</p>
+
+            <h2>4. Data Security</h2>
+
+            <p>We take reasonable measures to protect the security of your information. However, please note that no method of electronic storage or transmission is 100% secure. While we strive to use commercially acceptable means to protect your data, we cannot guarantee absolute security.</p>
+
+            <h2>5. Changes to the Privacy Policy</h2>
+
+            <p>We reserve the right to modify or update this Privacy Policy from time to time. We encourage you to review this policy periodically for any changes. Your continued use of the "Better Daily" app after any modifications to the Privacy Policy will signify your acceptance of the updated terms.</p>
+
+            <h2>6. Contact Us</h2>
+
+            <p>If you have any questions or concerns, please contact us at chasingrain22@gmail.com </p>
+        </div>
+    )
+};
---
+++ app/terms/page.tsx
@@ -0,0 +1,37 @@
+export default function Terms() {
+    return (
+        <div className="mx-5 mt-5 md:mx-10 md:mt-10 space-y-2">
+            <h1>Terms and Conditions</h1>
+
+            <h2>1. Platform License</h2>
+            <p>{`Better Daily is licensed, not sold, to you by ChasingRain Pte Ltd, "Company" hereafter , for use strictly in accordance with these Terms and Conditions ("Agreement"). By accessing or using the platform, you agree to be bound by this Agreement. If you do not agree to these terms, you may not use the platform.`}</p>
+
+            <h2>2. User Conduct</h2>
+            <p>By using the platform, you agree to use it only for lawful purposes and in a manner that does not infringe upon the rights of others or restrict or inhibit their use and enjoyment of the platform. You must not use the platform in any way that may cause damage to the platform or impair its availability or accessibility.</p>
+
+            <h2>3. Intellectual Property</h2>
+            <p>The platform and its original content, features, and functionality are owned by the Company and are protected by international copyright, trademark, patent, trade secret, and other intellectual property or proprietary rights laws. You may not modify, reproduce, distribute, create derivative works, publicly display, or perform any content or materials from the platform without the express written permission of the Company.</p>
+
+            <h2>4. Privacy</h2>
+            <p>Your privacy is important to us. Please refer to the Privacy Policy for the platform which explains how we collect, use, and disclose your information.</p>
+
+            <h2>5. Disclaimer of Warranty</h2>
+            <p>The platform is provided "as is" and without warranties of any kind, whether expressed or implied. The Company does not guarantee that the platform will be error-free, secure, or available at all times. Your use of the platform is at your sole risk.</p>
+
+            <h2>6. Limitation of Liability</h2>
+            <p>In no event shall the Company be liable for any indirect, incidental, special, consequential, or punitive damages arising out of or relating to your use of the platform. The Company's total liability to you for any damages, losses, or causes of action shall not exceed the amount paid by you, if any, for accessing or using the platform.</p>
+
+            <h2>7. Termination</h2>
+            <p>The Company may terminate or suspend your access to the platform at any time, without prior notice or liability, for any reason whatsoever, including without limitation if you breach this Agreement.</p>
+
+            <h2>8. Governing Law</h2>
+            <p>This Agreement shall be governed by and construed in accordance with the laws of [insert governing jurisdiction]. Any disputes arising under or in connection with this Agreement shall be subject to the exclusive jurisdiction of the courts in [insert jurisdiction].</p>
+
+            <h2>9. Severability</h2>
+            <p>If any provision of this Agreement is found to be invalid or unenforceable, that provision shall be deemed severed from this Agreement and shall not affect the validity and enforceability of the remaining provisions.</p>
+
+            <h2>10. Changes to the Terms and Conditions</h2>
+            <p>The Company reserves the right to modify or replace these Terms and Conditions at any time. Your continued use of the platform after any such changes constitutes your acceptance of the new Terms and Conditions.</p>
+        </div>
+    )   
+};
