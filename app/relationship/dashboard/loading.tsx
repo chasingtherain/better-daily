d3396ee1b4cc7b6c01e65ae307786324b2830b49
ChasingRain <junpeng94@gmail.com>
2023-07-24 17:11:33 +0800

    added loading file for love lang pages

--- app/relationship/dashboard/loading.tsx
+++ app/relationship/dashboard/loading.tsx
@@ -6,6 +6,7 @@ export default function Loading() {
     
     return (
         <div className="flex flex-col items-center gap-5 mt-10 md:mt-20 px-12 py-12">
+            <LoadingCards num={4} />
             <Skeleton className={`animate-pulse h-5 w-[180px] md:w-[320px] dark:bg-slate-200 bg-slate-400 rounded-[4px]`}/>
             <Skeleton className="animate-pulse dark:bg-slate-200 bg-slate-400 h-16 w-[180px] mb-2 md:w-[320px]" />
             
