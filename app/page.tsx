16a0b229626765a85fcf9c037ce1525fdbb3b758
ChasingRain <junpeng94@gmail.com>
2024-02-04 17:18:36 +0800

    removed dynamic greetings

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,11 @@ import { quotes } from "../data/quotes"
 import Checklist from "@/components/Checklist"
 import prisma from "@/lib/prisma"
 import { currentDate } from "@/utils/date"
-import { helloData } from "@/data/hello"
 import { getServerSession } from "next-auth"
 import { authOptions } from "@/pages/api/auth/[...nextauth]"
 import { generateRand } from "@/utils/numberGenerator"
 
 export default async function Page() {
-    
-    const greeting = helloData.filter(record => record.id === generateRand(helloData.length))[0]?.hello 
     const quote = quotes.filter(quote => quote.id === generateRand(quotes.length))[0]
     let focusList = null;
     let existingRecord;
@@ -44,7 +41,7 @@ export default async function Page() {
     if (session) {
       return (
         <div className="h-screen border-2 border-transparent">
-            <p className="text-xl md:text-[35px] mt-20 mb-4 text-center">{`${greeting ?? 'Hello'}, ${session?.user?.name ?? ""}`}</p>
+            <p className="text-xl md:text-[35px] mt-20 mb-4 text-center">{`Hey, ${session?.user?.name ?? ""}`}</p>
             <p className="text-xl md:text-3xl leading-normal font-serif tracking-normal text-center">Get Better Daily.</p>
             <div className="my-16 md:my-28 mx-[5%] md:mx-[25%] text-center">
                 <p className="text-xl md:text-4xl leading-normal font-serif tracking-wide italic my-8">{`“${quote?.text ?? "If you are working on something that you really care about, you don’t have to be pushed. The vision pulls you."}”`}</p>
